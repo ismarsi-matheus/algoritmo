2c15a4b81fc6a2dae42e531ccf5baf19929c3781
ismarsi-matheus <matheusleo.senac@gmail.com>
2024-05-23 11:57:22 -0300

    aula sobre operadores lógicos

---
+++ aula java/operadoresaritmeticos.js
@@ -0,0 +1,52 @@
+//string
+//boolan
+//int
+//float
+
+var texto = "aqui eu tenho uma string "
+var verdadeirofalso = false 
+var quebrado = 20.2
+var inteiro = 29
+
+console.log(1 + 1)
+
+var numero1 =10
+var numero2 = 14
+console.log("soma de valores:",numero1 + numero2)
+console.log("subtração de valores:",numero1 - numero2)
+console.log("multiplicação:",numero1 * numero2)
+console.log("divisão de valores:",numero1 / numero2)
+
+var soma = numero1 +numero2 
+var subtracao =  numero1 - numero2
+var multiplicacao = numero1 * numero2
+var divisao =numero1 /numero2 
+
+console.log("soma de valores:", soma )
+console.log("subtração de valores:",subtracao)
+console.log("multiplicação de valores:",multiplicacao)
+console.log("divisão de valores:",divisao)
+
+
+var numero=1
+var n1=numero*1
+var n2 =numero*2 
+var n3= numero*3
+var n4=numero*4
+var n5=numero*5
+var n6=numero*6
+var n7=numero*7
+var n8=numero*8
+var n9=numero*9
+var n10= numero*10
+
+console.log(numero,"x 1=",n1,)
+console.log(numero,"x 2=",n2)
+console.log(numero,"x 3 =",n3)
+console.log(numero,"x 4=",n4)
+console.log(numero,"x 5 =",n5)
+console.log(numero,"x 6=",n6)
+console.log(numero,"x 7=",n7)
+console.log(numero,"x 8=",n8)
+console.log(numero,"x 9=",n9)
+console.log(numero,"x10=",n10) 
---
+++ aula java/operadoreslogicos.js
@@ -0,0 +1,15 @@
+/**
+ * atribuicao      |=
+ * adiçao          |+
+ * subtracao       | -
+ * divisao         |/
+ * multiplicacao   |*
+ * negacao         |!false |true 
+ * igual           |==|3 ==3|true 
+ * diferente       |!=|
+ * maior           |>|4>3|true 
+ * menor           |<|4<3|false 
+ * maior igual     |>=|4>=4|true
+ * menor igual     |<=|5<= 5 |true 
+ * 
+ */
